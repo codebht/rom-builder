8a82871af13c7c8ecf60c6f5a16bf597009a10b9
codebht <bhavitpal1412@gmail.com>
2026-09-10 19:24:08 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-# write commands if you want to clone some repos
+# Device Tree
+git clone https://github.com/codebht/android_device_micromax_yogurt.git -b lineage-23.2 device/micromax/yogurt
+
+# Vendor Tree
+git clone https://github.com/codebht/android_vendor_micromax_yogurt.git -b lineage-23.2 vendor/micromax/yogurt
+
+# Kernel Tree
+git clone https://github.com/codebht/android_kernel_micromax_yogurt.git -b android-10.0 kernel/micromax/yogurt
